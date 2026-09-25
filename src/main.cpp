2dfa21e05a89fa191a9d5334f4b155da02099efd
MaxiHartmann <maxi.hartmann207@gmail.com>
2026-09-25 21:03:35 +0200

    basic triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+
+const char* vertexShaderSource = R"(
+#version 330 core
+
+layout(location = 0) in vec3 position;
+
+void main()
+{
+    gl_Position = vec4(position, 1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+
+uniform vec3 uColor;
+
+out vec4 color;
+
+void main()
+{
+    color = vec4(uColor, 1.0);
+}
+)";
 
 int main()
 {
@@ -9,6 +34,13 @@ int main()
         std::cerr << "Failed to initialize GLFW\n";
         return 1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(
+            GLFW_OPENGL_PROFILE,
+            GLFW_OPENGL_CORE_PROFILE
+            );
 
     GLFWwindow* window =
         glfwCreateWindow(800, 600, "Matrix", nullptr, nullptr);
@@ -22,10 +54,77 @@ int main()
 
     glfwMakeContextCurrent(window);
 
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW\n";
+        return 1;
+    }
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    
+    unsigned int VAO;
+    unsigned int VBO;
+    
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(vertices),
+        vertices,
+        GL_STATIC_DRAW
+    );
+    
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(float),
+        nullptr
+    );
+    
+    glEnableVertexAttribArray(0);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    
+    unsigned int shaderProgram = glCreateProgram();
+    
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    
+    glLinkProgram(shaderProgram);
+
+    int colorLocation = glGetUniformLocation(shaderProgram, "uColor");
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.02f, 0.02f, 0.02f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glUniform3f(colorLocation, 0.2f, 0.4f, 1.0f);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
